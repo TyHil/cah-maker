3f6e663b7218302642d2d6c585fa5cd9275baba6
TyHil <gotyhill@gmail.com>
2023-08-14 04:33:36 +0000

    Tab icon

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/* Tab Icon */
+
+const tabicon = document.querySelector('link[rel="icon"]');
+window
+  .matchMedia('(prefers-color-scheme: light)')
+  .addEventListener('change', function(event) {
+    if (event.matches) {
+      tabicon.href = './tabicon-light.png';
+    } else {
+      tabicon.href = './tabicon.png';
+    }
+  });
+
+
+
 /* Load SVGs */
 
 fetch('/images/white-front.svg')
