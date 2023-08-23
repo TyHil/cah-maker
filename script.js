000c88f284fe45923763a5d503fc80a976a28e5a
TyHil <gotyhill@gmail.com>
2023-08-23 23:15:35 +0000

    Tabicon style update

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 /* Tab Icon */
 
-const tabicon = document.querySelector('link[rel="icon"]');
-window
-  .matchMedia('(prefers-color-scheme: light)')
-  .addEventListener('change', function(event) {
-    if (event.matches) {
-      tabicon.href = 'cah-maker/tabicon-light.png';
-    } else {
-      tabicon.href = 'cah-maker/tabicon.png';
-    }
-  });
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+  document.querySelector('link[rel="icon"]').href = 'cah-maker/tabicon-light.png';
+}
 
 
 
