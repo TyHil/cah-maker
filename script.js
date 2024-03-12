3102dd711cbbf1fabeef9d005f38a188a8c470b6
Tyler Hill <gotyhill@gmail.com>
2024-03-12 00:57:33 -0500

    Published style guide

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* Tab Icon */
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
-  document.querySelector('link[rel="icon"]').href = 'cah-maker/tabicon-light.png';
+  document.querySelector('link[rel="icon"]').href = '/cah-maker/tabicon-light.png';
 }
 
 /* Load SVGs */
@@ -101,3 +101,49 @@ document.getElementById('file').addEventListener('change', function () {
   };
   reader.readAsText(this.files[0]);
 });
+
+/* Font not available */
+//From: https://stackoverflow.com/a/62687060
+
+const container = document.createElement('span');
+container.innerHTML = Array(100).join('wi');
+container.style.cssText = [
+  'position:absolute',
+  'width:auto',
+  'font-size:128px',
+  'left:-99999px'
+].join(' !important;');
+
+function getWidth(fontFamily) {
+  container.style.fontFamily = fontFamily;
+  document.body.appendChild(container);
+  width = container.clientWidth;
+  document.body.removeChild(container);
+  return width;
+}
+
+// Pre compute the widths of monospace, serif & sans-serif
+// to improve performance.
+const monoWidth = getWidth('monospace');
+const serifWidth = getWidth('serif');
+const sansWidth = getWidth('sans-serif');
+
+function isFontAvailable(font) {
+  return (
+    monoWidth !== getWidth(font + ',monospace') ||
+    sansWidth !== getWidth(font + ',sans-serif') ||
+    serifWidth !== getWidth(font + ',serif')
+  );
+}
+
+if (!isFontAvailable('HelveticaNeue')) {
+  const p = document.createElement('p');
+  p.innerText = "Looks like you don't have the right font installed. Download and install it from ";
+  const a = document.createElement('a');
+  a.href = 'https://fontsgeek.com/fonts/helveticaneue-bold';
+  a.target = '_blank';
+  a.rel = 'noopener';
+  a.innerText = 'Fonts geek';
+  p.appendChild(a);
+  createToast(p, { permanent: true, htmlText: true });
+}
